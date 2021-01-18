127710f8f2f2c03406dd45e31f4e014dfd292694
Adlane <50279061+aaadlane@users.noreply.github.com>
2021-01-18 17:01:36 +0100

    javascript working, need to review the design

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,17 @@
 players = [];
-colors = ["Red", "Blue"];
-
+nbrPerGroup = Number;
 
 let setplayers = () => {
-    if(localStorage.getItem('Players') === null){
+    if (localStorage.getItem('Players') === null) {
         players = [];
 
-    }else {
+    } else {
         players = localStorage.getItem('Players').split(',');
     }
 }
 setplayers()
-console.log("Players = ", players);
-console.log(localStorage);
 
-
-addPlayer = function() {
+addPlayer = function () {
     playerName = document.querySelector('#character').value;
     if (playerName !== "") {
         if (players.indexOf(playerName) == -1) {
@@ -23,6 +19,7 @@ addPlayer = function() {
             localStorage.setItem("Players", players);
         }
     }
+    console.log('nbr per groupe = ', nbrPerGroup);
     document.querySelector('#character').value = "";
     document.querySelector('#character').focus();
 };
@@ -31,4 +28,44 @@ addPlayer = function() {
 let clearAll = () => {
     players = [];
     localStorage.removeItem('Players');
+}
+
+// Afficher la liste des joueurs
+let display_players = () => {
+    if (players.length > 0) {
+        pl = "Player List: ";
+        for (i = 0; i < players.length; i++) {
+            pl += players[i] + ", ";
+        }
+        pl += ",";
+    } else {
+        pl = "Ajoutez des noms au tableau";
+    }
+    document.querySelector("#players_list").innerHTML = pl.split(", ,")[0];
+}
+display_players();
+
+
+
+let groupDisplay = () => {
+    const nbrPerGroup = document.querySelector('#nbrPerGroup').value
+    const container = document.querySelector("#Groups")
+
+    while (players.length > 0) {
+        const div = document.createElement('div');
+        div.classList.add("group")
+        div.innerHTML = `This group contains : <br/>`
+        const p = document.createElement('p');
+        for (let i = 0; i < nbrPerGroup; i++) {
+            if (players.length === 0) {
+                break
+            }
+            const span = document.createElement('span');
+            const index = Math.floor(Math.random() * players.length)
+            span.innerHTML = `${players[index]} <br/>`
+            div.appendChild(span)
+            players.splice(index, 1)
+        }
+        container.appendChild(div)
+    }
 }
